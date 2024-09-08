fbbfaf6f2e710db55f388d2a65658566e4185b35
Pool Golez <loop.edward@gmail.com>
2024-09-08 10:26:19 +0800

    Integrate chat history on chatbot

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ from rag import ask
 print("Refresh...")
 QUERIES_HISTORY = "queries_history"
 ANSWERS_HISTORY = "answers_history"
+CHAT_HISTORY = "chat_history"
 
 st.header("LangChain Docs Assistant")
 if QUERIES_HISTORY not in st.session_state:
     st.session_state[QUERIES_HISTORY] = []
-
 if ANSWERS_HISTORY not in st.session_state:
     st.session_state[ANSWERS_HISTORY] = []
-query = st.text_input("How can I help you?")
+if CHAT_HISTORY not in st.session_state:
+    st.session_state[CHAT_HISTORY] = []
 
+query = st.text_input("How can I help you?")
 if query:
     with st.spinner("Thinking..."):
-        response = ask(query)
+        response = ask(query, history=st.session_state[CHAT_HISTORY])
         st.session_state[QUERIES_HISTORY].append(query)
         st.session_state[ANSWERS_HISTORY].append(response)
+        st.session_state[CHAT_HISTORY].append(("human", query))
+        st.session_state[CHAT_HISTORY].append(("ai", response))
 
 if st.session_state[ANSWERS_HISTORY]:
     for query, response in zip(st.session_state[QUERIES_HISTORY], st.session_state[ANSWERS_HISTORY]):
--- rag.py
+++ rag.py
@@ -1,8 +1,10 @@
 import os
+from typing import Any, Dict, List
 
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings import OllamaEmbeddings
@@ -11,16 +13,25 @@ from langchain_pinecone import PineconeVectorStore
 load_dotenv()
 
 
-def ask(query: str):
+def ask(query: str, history: List[Dict[str, Any]] = []):
     embeddings = OllamaEmbeddings(model="all-minilm")
     vector_store = PineconeVectorStore(index_name=os.getenv("PINECONE_INDEX"), embedding=embeddings)
     chat = ChatOllama(model="llama3")
 
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat,
+        retriever=vector_store.as_retriever(),
+        prompt=rephrase_prompt)
+
     qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat, qa_chat_prompt)
-    qa = create_retrieval_chain(retriever=vector_store.as_retriever(), combine_docs_chain=stuff_documents_chain)
+    qa = create_retrieval_chain(retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain)
 
-    result = qa.invoke({"input": query})
+    result = qa.invoke({
+        "input": query,
+        "chat_history": history
+    })
     return result['answer']
 
 
